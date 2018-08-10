b87e78f4ee5de947acdaa0e62c1b8921a835f2ad
hurtn <nick.hurt@gmail.com>
2018-08-10 17:51:10 +0100

    Add files via upload

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import platform
 print("Python version: {0}".format(platform.python_version()))
 
 import sys, os.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname( __file__ ), 'acmeenv/Lib/site-packages')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname( __file__ ), 'acmevenv/Lib/site-packages')))
 
 import requests
 import json
@@ -15,16 +15,41 @@ import base64
 import hmac
 import hashlib
 
+########## Config required - enter your details in this section
 
-# this section takes the input binding which is the filename in the message from the queue and returns a URL which we can pass to the Vision service
-STORAGE_ACCOUNT_NAME = 'receiptstogo'
-STORAGE_ACCOUNT_KEY = 'EVIFTsGJZ2AncYB7rhIQx/bIEJl32dXSC82KPDERL0bZPlj6YIy82Af5wmoemdVxIMmLNNcsenaY+53WjnYMkA=='
-CONTAINER_NAME = 'image-drop'
+# Enter your storage account name, key and container name here
+STORAGE_ACCOUNT_NAME = '<storage account>'
+STORAGE_ACCOUNT_KEY = '<storage key>'
+CONTAINER_NAME = '<container name>'
 AZURE_STORAGE_VERSION = "2015-12-11"
+
+# Enter your store search url and api key
+store_search_url = "<store search url>"
+search_headers = {'api-key':'<search api key>'}
+
+# Enter your product search url
+product_search_url = "<product search url"
+
+# Vision API configuration
+# Replace <Subscription Key> with your valid subscription key.
+subscription_key = "<Vision API Key>"
+assert subscription_key
+
+# You must use the same region in your REST call as you used to get your
+# subscription keys. For example, if you got your subscription keys from
+# westus, replace "westcentralus" in the URI below with "westus".
+#
+# Free trial subscription keys are generated in the westcentralus region.
+# If you use a free trial subscription key, you shouldn't need to change
+# this region.
+vision_base_url = "https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/"
+
+############ end of config
 
 #read in the filename from the queue message
 blob_name = open(os.environ['inputMessage']).read()
 
+#build the SAS URL
 st= str(datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ"))
 se= str((datetime.utcnow() + timedelta(hours=1)).strftime("%Y-%m-%dT%H:%M:%SZ"))
 
@@ -51,22 +76,11 @@ blob_url = "https://{0}.blob.core.windows.net/{1}/{2}?{3}".format(
          blob_name,
          urllib.urlencode(querystring) )
 
-print ("Blob SAS URL: " + blob_url)
-#blob_url = urllib.quote(blob_url.encode('utf8'))
-#print ("Encoded SAS URL: " + blob_url)
+print ("Blob SAS URL created to access " + blob_name)
 
-# Replace <Subscription Key> with your valid subscription key.
-subscription_key = "4ba66f7ec8c448d59f1d55c4c6289d54"
-assert subscription_key
+# Computer Vision
 
-# You must use the same region in your REST call as you used to get your
-# subscription keys. For example, if you got your subscription keys from
-# westus, replace "westcentralus" in the URI below with "westus".
-#
-# Free trial subscription keys are generated in the westcentralus region.
-# If you use a free trial subscription key, you shouldn't need to change
-# this region.
-vision_base_url = "https://westus2.api.cognitive.microsoft.com/vision/v2.0/"
+# Build the request to the vision service
 
 text_recognition_url = vision_base_url + "recognizeText"
 
@@ -97,21 +111,15 @@ while "recognitionResult" not in analysis:
     analysis = response_final.json()
     time.sleep(1)
 
-    
 
-
-# In[4]:
-
-
-#get the snippets of text from the image
+# Extract the snippets of text from the image
 snippets = [(line["boundingBox"], line["text"])
   for line in analysis["recognitionResult"]["lines"]]
-#For this purpose assume store name is at the top of the receipt
+
+# Search for store
+# For this purpose assume store name is at the top of the receipt
 store_name = (snippets[0][1])
 print("Possible store name "+store_name)
-#First try a specific search
-store_search_url = "https://acme-demo-search.search.windows.net/indexes/azuresql-index/docs/search?api-version=2017-11-11"
-search_headers = {'api-key':'E41B60B5AC5DE691B6DC2C9ABB1C2CEC'}
 
 #override this for testing purposes
 #store_name = "John Levis"
@@ -128,7 +136,7 @@ def search_stores (p_store):
     search_response = requests.post(
         store_search_url, headers=search_headers, params=params, json=search_data)
     p_result = search_response.json()["value"]
-    print(p_result) 
+    #print(p_result) 
     return p_result
 
 search_result = search_stores(store_name)
@@ -148,8 +156,8 @@ else:
     
     
 ## Search for products    
-# specify the search url 
-product_search_url = "https://acme-demo-search.search.windows.net/indexes/azuresql-index-products/docs/search?api-version=2017-11-11"
+
+regex = r".[\s-]*[\d]*[\s-]*(\.[\s-]*\d{2})"
 
 #define a function so we can repeat the search if no match is found the first time
 def search_products (p_product):
@@ -165,17 +173,27 @@ def search_products (p_product):
     #print(p_result) 
     return p_result
 
-regex = r".[\s-]*[\d]*[\s-]*(\.[\s-]*\d{2})"
+previous_search = ''
+previous_snippet =''
 for snippet in snippets:
     #if we find a match then the previous element may be a product name 
-    if  re.search(regex, snippet[1]):
+    if  re.search(regex, snippet[1]) and \
+        previous_snippet.find('TOTAL')<0  and \
+        previous_snippet.find('CASH')<0 and \
+        previous_snippet.find('3 FOR')<0:
+
         print("Searching for product "+ previous_snippet)
-        search_result = search_products(previous_snippet)
+        
+        if previous_snippet != previous_search: 
+          search_result = search_products(previous_snippet)
+          #sleep(10)
+          previous_search = previous_snippet          
+
         if search_result:
-            print("Product is "+search_result[0]["Product"]+", ID "+search_result[0]["Id"])
+            print("Product found: "+search_result[0]["Product"]+", ID "+search_result[0]["Id"])
         else:
             print("Product could not be found ")
 
     else: #no match found keep track of the previous item
-        if snippet[1].isupper(): # only keep track of the uppercase words to avoid false positives
-            previous_snippet = snippet[1]        
+        if snippet[1].isupper():
+            previous_snippet = snippet[1]  
